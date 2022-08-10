11a2ad1f1055714d330e2d9d57c5c7b622c4e249
BMD63 <102309619+BMD63@users.noreply.github.com>
2022-08-10 11:32:30 +0400

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,29 +1,37 @@
-function typeselect() {
-    let typsel = document.getElementById('type').selectedIndex;
-    if(typsel==1){
-        let mssg=document.getElementById('select').innerHTML='<strong>Проект интерьера</strong>';
-        document.getElementById('table').style.display='none';
+let typsel = document.getElementById('type');
+function typeselect() { 
+    if(typsel.selectedIndex==2){
+        document.getElementById('select').innerHTML='<strong>Архитектурный проект здания</strong>';
+        document.getElementById('table').style.display='block';
     }
         else{
-            let mssg=document.getElementById('select').innerHTML='<strong>Архитектурный проект здания</strong>';
-            document.getElementById('table').style.display='block';
+        document.getElementById('select').innerHTML='<strong>Проект интерьера</strong>';
+        document.getElementById('table').style.display='none';
         }
     }
+    const check=document.getElementsByName('fl');
+    const checkarea=document.getElementsByName('fln');
 function area1() {
-    let check=document.getElementsByName('fl');
-    let checkarea=document.getElementsByName('fln');
     for(let i=0; i<check.length; i++){
-        console.log(i);
-        console.log(check[i].checked);
+        /* console.log(i);
+        console.log(check[i].checked); */
         if(check[i].checked){
             // console.log(checkarea[i]);
-            console.log(document.getElementsByName('fln')[i]); 
-            // checkarea[i].style.display='block';  
-            document.getElementsByName('fln')[i].style.display='table-cell';
-            
+            // console.log(document.getElementsByName('fln')[i]); 
+            checkarea[i].style.display='block';  
+            // document.getElementsByName('fln')[i].style.display='table-cell';
         }
         else{
             checkarea[i].style.display='none'
-        }
+            }
+        } 
     } 
-} 
+function resetform(){
+    // location.reload();
+    document.getElementById("calculator").reset();
+    /* console.log("test")
+    console.log(document.getElementById('type').selectedIndex); */
+    typeselect();
+    document.getElementById('select').innerHTML='';
+    area1()
+}
